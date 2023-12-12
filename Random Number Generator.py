57658839c59d0dd8215ce93c3b7d34bc817d1740
RonaldoRV <107870806+RonaldoRV@users.noreply.github.com>
2023-12-12 13:32:51 -0500

    Add files via upload

---
+++ Random Number Generator.py
@@ -0,0 +1,20 @@
+import random
+
+""""
+--Welcome to Random Number Generator--
+
+"""
+
+x = random.randint(1,1000)
+
+print(f"This is your random number: {x}")
+
+# Lets create a random number using functions
+def randoms():
+    xx = int(input("Enter a number: "))
+
+    randomn = xx - 20 * 80 - (12 + 5) 
+
+    print(f"This is your random number {randomn}")
+
+randoms()
